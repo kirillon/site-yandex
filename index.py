5e2e7db93f044b72c6eaaff7d4ff0f913b626da1
kirillon <kirilminecraft@gmail.com>
2022-02-04 22:58:20 +0300

    //kirillon

--- index.py
+++ index.py
@@ -22,25 +22,7 @@ def promotion():
             <p>Присоединяйся!</p>"""
 @app.route("/image")
 def image_mars():
-    return """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Привет, Марс!</title>
-</head>
-<body>
-    <h1>Жди нас, Марс!</h1>
-    
-        <p><img src="static/img/300px-OSIRIS_Mars_true_color.jpg" alt="qwerty" ></p>
-        <p>Вот она какая, красная планета.</p>
-    
-
-        
-    
-</body>
-</html>"""
+    return render_template("image_mars.html")
 
 
 if __name__ == '__main__':
